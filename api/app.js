379f9deec68300502df8a913ec9a14012e4928f9
cliffosa <cliffosa002@gmail.com>
2019-03-27 16:49:51 +0100

    setup app.js

---
+++ api/app.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+const app = express();
+
+const PORT = process.env.PORT || 8081;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+app.get('/', (req, res) => {
+  return res.status(200).send({
+    message: `Banka is a light-weight core banking application that powers banking operations like account
+creation, customer deposit and withdrawals. This app is meant to support a single bank, where
+users can signup and create bank accounts online, but must visit the branch to withdraw or
+deposit money..`
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening to port ${PORT}`);
+});

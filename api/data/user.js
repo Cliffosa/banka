5997951a207c6badad95e91eed371c44c199b8ea
cliffosa <cliffosa002@gmail.com>
2019-03-31 02:04:50 +0100

    created data modeling

---
+++ api/data/user.js
@@ -0,0 +1,48 @@
+let User = [
+  {
+    id: 1,
+    firstname: 'Mark',
+    lastname: 'Ade',
+    email: 'mark@gmail.com',
+    username: 'jomarknny',
+    password: 'mark123',
+    imageUrl: '',
+    type: 'client',
+    isAdmin: false
+  },
+  {
+    id: 2,
+    firstname: 'daniel',
+    lastname: 'cliff',
+    email: 'danny@gmail.com',
+    username: 'danny',
+    password: 'danny123',
+    imageUrl: '',
+    type: 'staff',
+    isAdmin: true
+  },
+  {
+    id: 3,
+    firstname: 'precious',
+    lastname: 'omo',
+    email: 'precious@gmail.com',
+    username: 'omo',
+    password: 'precious123',
+    imageUrl: '',
+    type: 'client',
+    isAdmin: false
+  },
+  {
+    id: 4,
+    firstname: 'cliff',
+    lastname: 'remedy',
+    email: 'remedy@gmail.com',
+    username: 'remedy',
+    password: 'remedy123',
+    imageUrl: '',
+    type: 'staff',
+    isAdmin: true
+  }
+];
+
+module.exports = User;

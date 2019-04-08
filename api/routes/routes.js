85a708c4e4afe087c424551975d4de7c8f36222c
cliffosa <cliffosa002@gmail.com>
2019-04-08 01:30:13 +0100

    ft(modify-user):implement delete and update user endpoints [Start #165170228]

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -4,8 +4,11 @@ import UserController from '../controllers/userController';
 const router = Router();
 
 router.post('/api/v1/auth/signup', UserController.registerUser);
+
 // user route
 router.get('/api/v1/users', UserController.fetchAllUsers);
 router.get('/api/v1/users/:id', UserController.fetchSingleUser);
+router.put('/api/v1/users/:id', UserController.updateUser);
+router.delete('/api/v1/users/:id', UserController.deleteUser);
 
 export default router;
